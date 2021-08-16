1551c5152eaebf2ffb37563f0e091d83e8ccb02c
Johannes Paasila <mail@jonne.org>
2021-08-16 17:08:34 +1000

    Make suspicious_processes more usable

--- suspicious_processes.py
+++ suspicious_processes.py
@@ -1,19 +1,18 @@
-import fileinput
-import sys
+#!/usr/bin/env python3
+
+from io import TextIOWrapper
 import json
 from typing import Any, List, NewType, Optional, Tuple, Union
-import logging
-from tqdm import tqdm
-from tqdm.contrib.logging import logging_redirect_tqdm
+import structlog
+import argparse
+import pandas as pd
 
-
-LOG = logging.getLogger(__name__)
 
 Process = NewType('Process', str)
 Parent = NewType('Parent', str)
 
 
-Event = Tuple[Process, Parent]
+Event = Tuple[pd.Timestamp, Process, Parent]
 
 
 def event(input: Union[str, bytes]) -> Event:
@@ -21,12 +20,13 @@ def event(input: Union[str, bytes]) -> Event:
     data = e['_source']['data']['win']['eventdata']
     process_name = data['newProcessName']
     parent_name = data['parentProcessName']
-    return (process_name, parent_name)
-
+    timestamp = pd.to_datetime(e['_source']['@timestamp'])
+    return (timestamp, process_name, parent_name)
     
-        
 
-def main(training_input: fileinput.FileInput, input: fileinput.FileInput) -> None:
+def main(training_input: TextIOWrapper, input: TextIOWrapper) -> None:
+
+    log = structlog.get_logger(detector='suspicious_processes')
 
     # Make set of all (process, parent) pairs seen in training data
     seen = set()
@@ -34,20 +34,23 @@ def main(training_input: fileinput.FileInput, input: fileinput.FileInput) -> Non
     # Load training data into model.
     for line in training_input:
         seen.add(event(line))
+    
+    log.info('training data loaded', known_pairs=len(seen))
    
     # Check for unseen (process, parent) pairs in main input
-    for line in tqdm(input, desc='events', maxinterval=1):
+    for line in input:
       
         e = event(line)
         if not seen.__contains__(e):
-            process, parent = e
-            LOG.info(f'anomaly: {parent} -> {process}')
-
+            timestamp, process, parent = e
+            ts = timestamp.to_pydatetime().isoformat()
+            log.info('suspicious process detected', time=ts, process=process, parent=parent)
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-    with logging_redirect_tqdm():
-        f = sys.argv[1]
-        main(fileinput.input(f), fileinput.input('-'))
-        LOG.info('done')
+    parser = argparse.ArgumentParser(description='Flag unknown process pairs in event stream')
+    parser.add_argument('--input', type=open, help='File containing event stream')
+    parser.add_argument('--training', type=open, help='File containing training data')
+    args = parser.parse_args()
+
+    main(args.training, args.input)
